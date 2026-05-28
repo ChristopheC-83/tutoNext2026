dfba5babc5007f6b49926466d42a59b4c7ae447f
kikisan <kiketdule@gmail.com>
2026-05-28 06:27:36 +0200

    fix

--- actions/clients.actions.ts
+++ actions/clients.actions.ts
@@ -5,6 +5,7 @@ import { processAvatarFile } from "@/lib/filesystem/img-processor";
 import { toClientDTO } from "@/lib/mappers/client.mapper";
 import { normalizePagination } from "@/lib/pagination/pagination";
 import { prisma } from "@/lib/prisma";
+
 import {
   clientSchema,
   clientUpdateSchema,
@@ -136,23 +137,25 @@ export async function getAllClientsAction({
     prisma.client.count({ where: { userId: user.userId } }),
   ]);
   //Refine data
-  const clients: ClientListItem[] = clientsRaw.map((client) => {
-    const mappedClient: ClientDTO = toClientDTO(client);
-    const invoicesCount = client.invoices.length;
-    const overdueCount = client.invoices.filter(
-      (inv) => inv.status === "OVERDUE",
-    ).length;
-    const totalAmount = client.invoices.reduce(
-      (sum, inv) => sum + inv.amount,
-      0,
-    );
-    return {
-      ...mappedClient,
-      invoicesCount,
-      overdueCount,
-      totalAmount,
-    };
-  });
+  const clients: ClientListItem[] = clientsRaw.map(
+    (client: (typeof clientsRaw)[number]) => {
+      const mappedClient: ClientDTO = toClientDTO(client);
+      const invoicesCount = client.invoices.length;
+      const overdueCount = client.invoices.filter(
+        (inv) => inv.status === "OVERDUE",
+      ).length;
+      const totalAmount = client.invoices.reduce(
+        (sum, inv) => sum + inv.amount,
+        0,
+      );
+      return {
+        ...mappedClient,
+        invoicesCount,
+        overdueCount,
+        totalAmount,
+      };
+    },
+  );
   //Return
   return {
     data: clients,
